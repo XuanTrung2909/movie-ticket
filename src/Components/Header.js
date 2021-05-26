6b21422311edfec4672fe6e340adb1eb28abc0a4
XuanTrung2909 <trung.nx290989@gmail.com>
2021-05-26 17:01:01 +0700

    upload project

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './../Assets/img/tix-logo.png'
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className="logo">
+        <img src={logo} alt="Tix.vn" />
+      </div>
+      <ul className="nav">
+
+      </ul>
+      <div className="sign">
+
+      </div>
+    </div>
+  )
+}
---
+++ src/Ulti/constants.js
@@ -0,0 +1,12 @@
+ export const NAVLIST = [
+  {
+    value: 0,
+    name: 'Phim',
+    path: '/phim'
+  },
+  {
+    value: 1,
+    name: 'Cụm Rạp',
+    path: '/cum-rap'
+  }
+]
